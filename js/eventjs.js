d6281c4fa6a69a4882390c6ab2a02bd6b653c4cd
Chung-Heewon <hwjeong2002@gmail.com>
2023-07-04 17:53:17 +0900

    7/4 java script

--- js/eventjs.js
+++ js/eventjs.js
@@ -65,8 +65,134 @@ window.addEventListener('load',function(){
     chkAllObj.addEventListener('click', (e)=>{
         //console.log(e.target.checked)
         chkobjs.forEach((chk)=>{
-            chk.checked=e.target.checked
+            chk.checked=e.target.checked //클릭되었을 때 할일을 미리 정해놓는 것(Event 처리방법)
         })
     })
     //--all checkbox객체 클릭되었을 때 END--
+
+    //--차량 대분류를 선택했을 때 할 일 START--
+    let carTypeObj=
+        document.querySelector('div.car>select.type'); //대분류
+
+    let carType2Obj=
+        document.querySelector('div.car>select.type2'); //중분류
+
+    carTypeObj.addEventListener('click', ()=>{
+        console.log('clicked')
+    })
+    carTypeObj.addEventListener('change',(e)=>{
+        console.log(e.target.value, 'changed')
+        switch(e.target.value){
+            case 'sedan':
+                type2 = [{name : '쏘나타',               id: 'sonata' },
+                         {name : '더 뉴 아반테',          id : 'avante'},
+                         {name : '디 올 뉴 그랜저',       id : 'grandure'},
+                         {name : '디 올 뉴 그랜저Hybrid', id : 'hybrid'}
+                ]
+                carType2Obj.innerHTML = '<option>선택하세요</option>' //=은 초기화
+                carType2Obj.innerHTML += '<option value ="'+type[0].id+'">'+type[0].name+ '</option>'
+                carType2Obj.innerHTML += '<option value ="'+type[1].id+'">'+type[1].name+ '</option>'
+                carType2Obj.innerHTML += `<option value = "${type[2].id}">${type[2].name}</option>`
+                carType2Obj.innerHTML += `<option value = "${type[3].id}">${type[3].name}</option>`
+                carType2Obj.style.display = 'inline-block'
+                break;
+            case 'suv':
+                type2 = [
+                    {name : '펠리세이드', id : 'palisade'}, 
+                    {name : '베뉴',      id : 'venu'}, 
+                    {name : '코나',      id : 'kona'}]
+                carType2Obj.innerHTML = '<option>'+'선택하세요'+'</option>'
+                type2.forEach((value, index)=>{
+                    carType2Obj.innerHTML+=
+                    `<option value=${value.id}>${value.name}</option>`
+                })
+                // carType2Obj.innerHTML += '<option>'+type2[0]+ '</option>'
+                // carType2Obj.innerHTML += '<option>'+type2[1]+ '</option>'
+                // carType2Obj.innerHTML += '<option>'+type2[2]+ '</option>'
+                carType2Obj.style.display = 'inline-block'
+                break;
+            case 'truck':
+                type3 = ['선택하세요','마이티', '더 뉴파비스', '엑시언트']
+                // carType2Obj.innerHTML = '<option>'+'선택하세요'+'</option>'
+                // carType2Obj.innerHTML += '<option>'+type3[0]+ '</option>'
+                // carType2Obj.innerHTML += '<option>'+type3[1]+ '</option>'
+                // carType2Obj.innerHTML += '<option>'+type3[2]+ '</option>'
+                // carType2Obj.style.display = 'inline-block'
+                let options = document.querySelectorAll('div.car>select.type2>option')
+                
+                //let options = carType2Obj.childNodes //carType2Obj라는 기준에서부터 자식들을 찾는다. 
+                //options.forEach((value)=>{
+                    //carType2Obj.removeChild(value)
+                //})
+                //
+                // carType2Obj.forEach((value)=>{
+                //     carType2Obj.removeChild(value)
+                // })
+                type3.forEach((value)=>{
+                    let optionObj = document.createElement('option')
+                    //optionObj.innerHTML = value
+                    let txtObj = document.createTextNode(value)
+                    optionObj.appendChild(txtObj)
+                    carType2Obj.appendChild(optionObj)
+                })
+
+                carType2Obj.style.display='inline-block'
+                break;
+                default:
+                    carType2Obj.style.display = 'none'
+
+        }
+    })
+    //--차량 대분류를 선택했을 때 할 일 END--
+
+    //--입력란에 키보드입력할 때 할 일 START --
+    //--DOMkeyboard입력요소 객체 찾기
+    const txtObj =
+        document.querySelector('div.keyboard>input[type=text]')
+    txtObj.addEventListener('keyup',(e)=>{
+        console.log(e.key, e.target.value)
+        if(e.key == 'Enter'){
+            this.alert('Enter를 입력하셨습니다.')
+        }else{
+            e.target.value = e.target.value.toUpperCase()
+        }
+        
+    })
+    //--입력란에 키보드입력할 때 할 일 END --
+
+    //--전송버튼 클릭할 때 할 일 START --
+    const btSubmit = 
+        document.querySelector('div.form>form>button')
+        btSubmit.addEventListener('click',()=>{
+            alert("전송버튼이 출력되었습니다.")
+        })
+    //TODO : 콘솔에 전송버튼이 클릭되었습니다. 출력
+
+    //--전송버튼 클릭할 때 할 일 END --
+
+    
+    //--폼의 submit이벤트 발생할 때 할 일 START --
+    const formObj = this.document.querySelector('div.form>form')
+    formObj.addEventListener('submit',(e)=>{
+        this.alert('submit이벤트가 발생하였습니다.')
+        e.preventDefault()
+    })
+    //--폼의 submit이벤트 발생할 때 할 일 START --
+
+    //-- a객체의 click이벤트 발생할 때 할 일 START --
+    
+    //div객체
+    const linkDiveObj = document.querySelector('div.link')
+    linkDiveObj.addEventListener('click',(e)=>{
+        e.target.style.backgroundColor = 'yellow'
+    })
+
+    //a객체
+    const aObj = this.document. querySelector('div.link>a')
+    aObj.addEventListener('click', (e)=>{
+        this.alert('링크 클릭')
+        e.preventDefault() //a tag의 기본 event인 click event를 하지 말아라
+        e.stopPropagation() //이벤트의 전파를 중지하겠다.
+    })
+    //-- a객체의 click 이벤트 발생할 때 할 일 END --
 })
---
+++ js/signup.js
@@ -0,0 +1,58 @@
+//dom트리가 완성되고 
+//렌더링준비가 완료되었을때 widnow객체의 load이벤트가 발생한다. 
+window.addEventListener('load', ()=>{
+    //DOM 트리에서 form객체 찾기
+    const formObj= 
+    document.querySelector('body>form')
+
+    //아이디입력란 객체찾기
+    const inputIdObj =
+    document.querySelector('body>form.signup>input[id=id]')
+
+    //아이디중복확인버튼 객체 찾기
+    const btIdDupchk = 
+    document.querySelector('body>form.signup>button.iddupchk')
+
+    //가입버튼객체 찾기
+    const btSignup=
+    document.querySelector('body>form.signup>button.signup')
+    //아이디중복확인버튼-일반버튼,
+    btIdDupchk.addEventListener('click',()=>{
+        if(inputIdObj.value =='id1'){
+            this.alert('이미가입된 아이디입니다.')
+        }else{
+            btSignup.style.display = 'block'
+        }
+    }) 
+
+    //가입버튼-전송버튼 클릭 ->
+    //폼의 서브밋이벤트발생
+
+    //--폼 서브밋이벤트발생시 할 일 START--
+    formObj.addEventListener('submit',
+                             (e)=>{
+        //아이디입력값이 'id1'인 경우
+        //이미사용중인 아이디입니다. 경고창을 보여준다. 
+        //아이디입력값이 'id1'가 아닌 경우
+        //가입버튼을 화면에 보여주기하기 
+        
+            //비밀번호1,2가 일치 확인
+            const pwdObj = document.querySelector('body>form.signup>input[id=pwd]')
+            const pwd1Obj =document.querySelector('#pwd1') 
+            if(pwdObj.value!=pwd1Obj.value){
+                alert('비밀번호가 일치하지 않습니다.')
+                pwdObj.focus();
+            }
+
+            e.preventDefault();
+        })
+    //--폼 서브밋이벤트발생시 할 일 End--
+    inputIdObj.addEventListener('focus',()=>{
+
+    
+    //--아이디입력란에 'focus'이벤트발생시 할 일 START --
+        btSignup.style.display='none'
+        //--가입버튼 사라지기
+    })
+    //--아이디입력란에 focus이벤트발생시 할 일 END --
+})
